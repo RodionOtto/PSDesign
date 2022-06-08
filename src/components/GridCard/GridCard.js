5ef32b6d0bd4f4fe53210f2d11a2b38ce8fa5fa9
Rodion Zhadanov <49521764+RodionOtto@users.noreply.github.com>
2022-06-08 15:27:13 +0300

    feat/added modal window that opens on click on a card of card list

--- src/components/GridCard/GridCard.js
+++ src/components/GridCard/GridCard.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './GridCard.css';
+import Slider from '../Slider/Slider';
+import ModalWindow from '../Modal/Modal';
 
 const GridCard = ({caseItem}) => {
+
+    const [modalActive, setModalActive] = useState(false);
+
     return(
+        <>
         <div 
             className="cases__grid-item" 
-            style={{backgroundImage: `url(${caseItem.img})`}}>
+            style={{backgroundImage: `url(${caseItem.img})`}}
+            onClick={() => setModalActive(true)}
+            >
             <h3 className="cases__name">{ caseItem.title }</h3>
             <p className="cases__description">{ caseItem.text }</p>
+
+            
         </div> 
+        <ModalWindow active={modalActive} setActive={setModalActive}>
+            <Slider />
+        </ModalWindow>
+        </>
     )
 }
 
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Slider.css';
+
+const Slider = () => {
+
+    return (
+        <>
+            
+        </>
+    )
+};
+
+export default Slider;
