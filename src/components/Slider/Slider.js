2bb996ca0d53650ebd57fbce8a9c0d2b0266a23b
Rodion Zhadanov <49521764+RodionOtto@users.noreply.github.com>
2022-06-09 09:38:17 +0300

    feat/imported Swiper library for slider and added it with simple style

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import "swiper/swiper.min.css";
+import "swiper/modules/pagination/pagination.min.css";
+import "swiper/modules/navigation/navigation.min.css";
 import './Slider.css';
 
 const Slider = () => {
 
     return (
-        <>
-            
-        </>
+            <Swiper
+            spaceBetween={30}
+            rewind={true}
+            pagination={{
+                dynamicBullets: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className='slider'
+            >
+                <SwiperSlide><img alt={'image'} src={require('../../images/cases-cover/case1.jpg')} /></SwiperSlide>
+                <SwiperSlide><img alt={'image'} src={require('../../images/cases-cover/case2.jpg')} /></SwiperSlide>
+                <SwiperSlide><img alt={'image'} src={require('../../images/cases-cover/case3.jpg')} /></SwiperSlide>
+            </Swiper>
     )
 };
 
