f524ca72a1919ee42162971725e752b26829f127
Rodion Zhadanov <49521764+RodionOtto@users.noreply.github.com>
2022-06-09 10:17:44 +0300

    hotfix/added modules and styles for slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation, Keyboard } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import "swiper/swiper.min.css";
 import "swiper/modules/pagination/pagination.min.css";
@@ -10,18 +10,26 @@ const Slider = () => {
 
     return (
             <Swiper
-            spaceBetween={30}
+            cssMode={true}
+            keyboard={{enabled: true}}
+            style={{
+                "--swiper-navigation-color": "#0081CA",
+                "--swiper-pagination-color": "#0081CA",
+            }}
+            slidesPerView={1}
+            spaceBetween={40}
             rewind={true}
             pagination={{
                 dynamicBullets: true,
+                clickable: true,
             }}
             navigation={true}
-            modules={[Pagination, Navigation]}
+            modules={[Pagination, Navigation, Keyboard]}
             className='slider'
             >
-                <SwiperSlide><img alt={'image'} src={require('../../images/cases-cover/case1.jpg')} /></SwiperSlide>
-                <SwiperSlide><img alt={'image'} src={require('../../images/cases-cover/case2.jpg')} /></SwiperSlide>
-                <SwiperSlide><img alt={'image'} src={require('../../images/cases-cover/case3.jpg')} /></SwiperSlide>
+                <SwiperSlide className='slide'><img className='slide-img' alt={'фото сделанного проекта'} src={require('../../images/cases-cover/case1.jpg')} /></SwiperSlide>
+                <SwiperSlide className='slide'><img className='slide-img' alt={'фото сделанного проекта'} src={require('../../images/cases-cover/case2.jpg')} /></SwiperSlide>
+                <SwiperSlide className='slide'><img className='slide-img' alt={'фото сделанного проекта'} src={require('../../images/cases-cover/case3.jpg')} /></SwiperSlide>
             </Swiper>
     )
 };
